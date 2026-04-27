250c9e1924dcc11e038d88f352e348107b0e0155
Thirulingam L <thiru665hd@gmail.com>
2026-04-27 09:30:40 +0530

    Fix playwright headless browser in CI

--- packages/demoqa/amazon/tests/conftest.py
+++ packages/demoqa/amazon/tests/conftest.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="session")
 def browser():
     playwright = sync_playwright().start()
-    browser = playwright.chromium.launch(headless=False, slow_mo= 500)
+    browser = playwright.chromium.launch(headless=True)
     yield browser
     browser.close()
     playwright.stop()
